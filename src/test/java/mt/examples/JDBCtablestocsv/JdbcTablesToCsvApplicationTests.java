75ba67df17163679fbc76532b6234d4edf97e0d4
mtzagkarakis <tzagasdog@gmail.com>
2020-01-17 12:54:04 +0200

    Initial commit

---
+++ src/test/java/mt/examples/JDBCtablestocsv/JdbcTablesToCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package mt.examples.JDBCtablestocsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTablesToCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
